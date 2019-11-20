c6af6615fbff54673e18653ab8a3d21f48ae7857
Andrew-Kot <55478660+Andrew-Kot@users.noreply.github.com>
2019-11-20 11:27:22 +0300

    Командная доработка

--- solar_input.py
+++ solar_input.py
@@ -7,9 +7,7 @@ from solar_objects import Star, Planet
 def read_space_objects_data_from_file(input_filename):
     """Cчитывает данные о космических объектах из файла, создаёт сами объекты
     и вызывает создание их графических образов
-
     Параметры:
-
     **input_filename** — имя входного файла
     """
 
@@ -37,13 +35,10 @@ def parse_star_parameters(line, star):
     """Считывает данные о звезде из строки.
     Входная строка должна иметь слеюущий формат:
     Star <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
-
     Здесь (x, y) — координаты зведы, (Vx, Vy) — скорость.
     Пример строки:
     Star 10 red 1000 1 2 3 4
-
     Параметры:
-
     **line** — строка с описание звезды.
     **star** — объект звезды.
     """
@@ -61,13 +56,10 @@ def parse_planet_parameters(line, planet):
     Предполагается такая строка:
     Входная строка должна иметь слеюущий формат:
     Planet <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
-
     Здесь (x, y) — координаты планеты, (Vx, Vy) — скорость.
     Пример строки:
     Planet 10 red 1000 1 2 3 4
-
     Параметры:
-
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
@@ -85,19 +77,19 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     Строки должны иметь следующий формат:
     Star <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
     Planet <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
-
     Параметры:
-
     **output_filename** — имя входного файла
     **space_objects** — список объектов планет и звёзд
     """
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
-            print(out_file, "%s %f %f %f %f %f %f" % (space_objects[0], space_objects[1], space_objects[2], space_objects[3], 
-            space_objects[4], space_objects[5], space_objects[6]))
-            # FIXME: should store real values
+            print(out_file, "%s %f %s %f %f %f %f %f" % (obj.type, obj.R, obj.color, obj.m, obj.x, obj.y, obj.Vx, obj.Vy))
 
 # FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
+#def statistics:
+    """Сохраняет статистику в файл stats.py.
+    Формат: t Vx Vy x y"""
+ 
 
 if __name__ == "__main__":
     print("This module is not for direct call!")
